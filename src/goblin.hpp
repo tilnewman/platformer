2096de855718c52a6e1c1f9eea849771daff4d88
Ziesche Til Newman <tilnewman@protonmail.com>
2024-08-20 07:37:44 -0600

    make monster virtual functions final

--- src/goblin.hpp
+++ src/goblin.hpp
@@ -25,15 +25,15 @@ namespace platformer
         virtual ~Goblin() override = default;
 
         // IMonster functions
-        const Harm avatarCollide(const sf::FloatRect & avatarRect) override;
-        const sf::FloatRect collisionRect() const override;
-        const sf::FloatRect attackCollisionRect() const override;
+        const Harm avatarCollide(const sf::FloatRect & avatarRect) final;
+        const sf::FloatRect collisionRect() const final;
+        const sf::FloatRect attackCollisionRect() const final;
 
       protected:
-        inline float walkSpeed() const override { return 40.0f; }
-        void playAttackSfx(Context & context) const override;
-        void playHurtSfx(Context & context) const override;
-        void playDeathSfx(Context & context) const override;
+        inline float walkSpeed() const final { return 40.0f; }
+        void playAttackSfx(Context & context) const final;
+        void playHurtSfx(Context & context) const final;
+        void playDeathSfx(Context & context) const final;
     };
 
 } // namespace platformer
