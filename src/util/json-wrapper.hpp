5c3d9d6bc08f1f87ab9330ccbc16bd330a26c7b4
Ziesche Til Newman <tilnewman@protonmail.com>
2024-08-29 08:01:28 -0600

    stop some windows warnings

--- src/util/json-wrapper.hpp
+++ src/util/json-wrapper.hpp
@@ -3,11 +3,15 @@
 //
 // json-wrapper.hpp
 //
+#ifdef WIN32
+#include "json.hpp"
+#else
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wswitch-enum"
 #pragma GCC diagnostic ignored "-Wdocumentation-unknown-command"
 #pragma GCC diagnostic ignored "-Wdocumentation-pedantic"
 #include "json.hpp"
 #pragma GCC diagnostic pop
+#endif
 
 #endif // JSON_WRAPPER_HPP_INCLUDED
