e168051cea7f428dd852d6f506293b29cddc3f2f
Ziesche Til Newman <tilnewman@protonmail.com>
2025-03-08 09:59:47 -0700

    added comments and fixed var names

--- src/subsystem/screen-layout.cpp
+++ src/subsystem/screen-layout.cpp
@@ -17,21 +17,23 @@ namespace bramblefore
         , m_wholeRect{}
     {}
 
-    void ScreenLayout::setup(const sf::Vector2u & windowSize)
+    void ScreenLayout::setup(const sf::Vector2u & t_windowSize)
     {
-        m_wholeSize = { static_cast<float>(windowSize.x), static_cast<float>(windowSize.y) };
+        m_wholeSize = { static_cast<float>(t_windowSize.x), static_cast<float>(t_windowSize.y) };
         m_wholeRect = { { 0.0f, 0.0f }, m_wholeSize };
     }
 
     float ScreenLayout::calScaleBasedOnResolution(
-        const Context & context, const float originalScale) const
+        const Context & t_context, const float t_scale) const
     {
-        const float originalResProduct{ std::sqrt(2056.0f * 1329.0f) };
+        // This is the resolution on my macbook originally used when starting the game.
+        // The values in settings.hpp work as is when this is the resolution.
+        const float original{ std::sqrt(2056.0f * 1329.0f) };
 
-        const float currentResProduct{ std::sqrt(static_cast<float>(
-            context.settings.video_mode.size.x * context.settings.video_mode.size.y)) };
+        const float current{ std::sqrt(static_cast<float>(
+            t_context.settings.video_mode.size.x * t_context.settings.video_mode.size.y)) };
 
-        return ((originalScale * currentResProduct) / originalResProduct);
+        return ((t_scale * current) / original);
     }
 
 } // namespace bramblefore
